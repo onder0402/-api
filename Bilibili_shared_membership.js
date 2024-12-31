113598b01073c5bb4b181d098926b9b9d594af78
Myatic <104758547@qq.com>
2025-01-01 01:27:06 +0800

    创建 Bilibili_shared_membership.js

---
+++ Bilibili_shared_membership.js
@@ -0,0 +1,7 @@
+var modifiedHeaders = $request.headers;
+
+// 根据您提供的数据填写具体的内容
+modifiedHeaders['Authorization'] = 'identify_v1 31b8c72c7c9903005f51e748ab3ba2c2CjB34X6S04ZSf1r8iIMkxX_fwP51U-dRFP1B1_vBC_uMs3_uLfQ7RdBUri6aCxxxdysSVk1zT2FWd0QzSGc5OTYzYUtsb3MyTFItWTJBOVgwWDRPUlBYN2ZyYnpUak1ubW12NzF2MTJnUGgzcFdWTGVMNml5ekh4NzFHYUstSnZxaVlFd0lZeHd3IIEC';
+modifiedHeaders['User-Agent'] = 'bili-inter/82300100 os/ios model/iPhone 15 Pro Max mobi_app/iphone_i osVer/18.2 network/2 grpc-objc-cronet/1.47.0 grpc-c/25.0.0 (ios; cronet_http)';
+
+$done({headers: modifiedHeaders});
